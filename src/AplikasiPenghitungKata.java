020af22c241f3c7220d982b00f6ff837963e284b
Hilmanaja <muhammadhilman1806@gmail.com>
2024-11-05 19:38:21 +0800

    nambah komponen GUI

--- src/AplikasiPenghitungKata.java
+++ src/AplikasiPenghitungKata.java
@@ -26,17 +26,111 @@ public class AplikasiPenghitungKata extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtAreaInput = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        btnCariKata = new javax.swing.JButton();
+        btnHitung = new javax.swing.JButton();
+        btnSimpan = new javax.swing.JButton();
+        lblJumlahKata = new javax.swing.JLabel();
+        lblJumlahKarakter = new javax.swing.JLabel();
+        lblJumlahKalimat = new javax.swing.JLabel();
+        lblJumlahParagraf = new javax.swing.JLabel();
+        txtCariKata = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Masukan Text");
+
+        txtAreaInput.setColumns(20);
+        txtAreaInput.setRows(5);
+        jScrollPane1.setViewportView(txtAreaInput);
+
+        jLabel2.setText("Cari Kata");
+
+        btnCariKata.setText("Cari");
+
+        btnHitung.setText("Hitung");
+
+        btnSimpan.setText("Simpan");
+
+        lblJumlahKata.setText("Jumlah Kata");
+
+        lblJumlahKarakter.setText("Jumlah Karakter");
+
+        lblJumlahKalimat.setText("Jumlah Kalimat");
+
+        lblJumlahParagraf.setText("Jumlah Paragraf");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblJumlahParagraf)
+                    .addComponent(lblJumlahKalimat)
+                    .addComponent(lblJumlahKarakter)
+                    .addComponent(lblJumlahKata)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnHitung)
+                        .addGap(35, 35, 35)
+                        .addComponent(btnSimpan))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 496, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(txtCariKata, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnCariKata)))
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(68, 68, 68)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCariKata)
+                    .addComponent(txtCariKata, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHitung)
+                    .addComponent(btnSimpan))
+                .addGap(18, 18, 18)
+                .addComponent(lblJumlahKata)
+                .addGap(18, 18, 18)
+                .addComponent(lblJumlahKarakter)
+                .addGap(18, 18, 18)
+                .addComponent(lblJumlahKalimat)
+                .addGap(18, 18, 18)
+                .addComponent(lblJumlahParagraf)
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +172,18 @@ public class AplikasiPenghitungKata extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCariKata;
+    private javax.swing.JButton btnHitung;
+    private javax.swing.JButton btnSimpan;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblJumlahKalimat;
+    private javax.swing.JLabel lblJumlahKarakter;
+    private javax.swing.JLabel lblJumlahKata;
+    private javax.swing.JLabel lblJumlahParagraf;
+    private javax.swing.JTextArea txtAreaInput;
+    private javax.swing.JTextField txtCariKata;
     // End of variables declaration//GEN-END:variables
 }
